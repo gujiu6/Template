76b9673ac32084eaa37f91d0dffdf8731613a5a5
gujiu6 <18273655445@163.com>
2026-08-23 11:40:35 +0800

    11

--- LinearAlgebraAndNumerical/GaussianEliminationAdd.cpp
+++ LinearAlgebraAndNumerical/GaussianEliminationAdd.cpp
@@ -1,61 +1,61 @@
+/*线性方程与规划
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXX=2e5;
-const double eps=1e-6;
-typedef long long ll;
+using i64 = long long;
+using ld = long double;
+constexpr ld eps = 1e-12;
 
-
-
-
-
-
-
-
-
-array<array<double,100>,100>mat;
-//高斯消元(加法方程)
-void gaussAdd(int n){
-	for(int i = 1; i <= n; i++){
-		int mx = i;
-		for(int j = 1; j <= n; j++){
-			if(j < i && fabs(mat[j][j]) >= eps){
-				continue;
-			}
-			if(fabs(mat[j][i]) > fabs(mat[mx][i])){
-				mx = j;
-			}
+//2.高斯消元:列主元
+template <class T = ld>
+optional<pair<bool, vector<T>>> gaussPivot(vector<vector<T>> a){
+	//Ax = b
+    int m = a.size(), n = a[0].size() - 1, rk = 0;
+    vector<int> pivot(n, -1);//第 col 个未知数的主元在哪一行
+    for (int col = 0; col < n && rk < m; col++) {
+		//部分主元选取
+        int row = rk;
+        for (int i = rk + 1; i < m; i++) {
+            if (fabsl(a[i][col]) > fabsl(a[row][col])) {
+                row = i;
+            }
+        }
+        if (fabsl(a[row][col]) <= eps) {
+            continue;
+        }
+        swap(a[row], a[rk]);
+		//主元行归一化
+        for (int j = n; j >= col; j--) {
+            a[rk][j] /= a[rk][col];
+        }
+		//用主元消掉其他所有行
+        for (int i = 0; i < m; i++) {
+            if (i == rk) {
+                continue;
+            }
+            T q = a[i][col];
+            for (int j = col; j <= n; j++) {
+                a[i][j] -= q * a[rk][j];
+            }
+        }
+		//记录主元并增加秩
+        pivot[col] = rk++;
+    }
+	//无解(0 == 常数)
+    for (int i = rk; i < m; i++) {
+        if (fabsl(a[i][n]) > eps) {
+            return nullopt;
+        }
+    }
+	vector<T> x(n);
+	for (int col = 0; col < n; col++){
+		if(pivot[col] != -1) {
+			x[col] = a[pivot[col]][n];
 		}
-		swap(mat[mx], mat[i]);
-		if(fabs(mat[i][i]) >= eps){
-			double tmp = mat[i][i];
-			for(int j = i; j <= n + 1; j++){
-				mat[i][j] /= tmp;
-			}
-			for(int j = 1; j <= n; j++){
-				if(i != j){
-					double rate = mat[j][i] / mat[i][i];
-					for(int k = i; k <= n + 1; k++){
-						mat[j][k] -= mat[i][k] * rate;
-					}
-				}
-			}
-		}
-	}
-}
-
-//判断  1:唯一解  0:多解  -1:无解
-int pd(int n){
-	int ans = 1;
-    for(int i = 1; i <= n; i++){
-		if(fabs(mat[i][i]) < eps && fabs(mat[i][n+1]) >= eps){
-			ans = -1;
-			break;
-		}
-		if(fabs(mat[i][i]) < eps){
-			ans = 0;
-		}
-    }	
-    return ans;
+    }
+	if(rk == n) return {true, x};//唯一解
+    return {false, x};//无穷多解,返回一组特解
 }
 
 
