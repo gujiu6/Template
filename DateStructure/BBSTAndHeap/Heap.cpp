5a3e525305b4f67fb2bdf9b8a50132e7cb3a3466
gujiu6 <18273655445@163.com>
2026-08-24 22:59:47 +0800

    11

--- DateStructure/BBSTAndHeap/Heap.cpp
+++ DateStructure/BBSTAndHeap/Heap.cpp
@@ -8,50 +8,6 @@ using namespace std;
 
 
 
-template <typename T=int>
-class DualHeap{
-private:
-    priority_queue<T>lo;
-    priority_queue<T,vector<T>,greater<T>>hi;
-    unordered_map<T,int>dead;
-    int _k=0,sz=0,lo_sz=0,hi_sz=0;
-    void clean(){
-        while(!lo.empty()&&dead[lo.top()]>0){
-            dead[lo.top()]--;
-            lo.pop();
-        }
-        while(!hi.empty()&&dead[hi.top()]>0){
-            dead[hi.top()]--;
-            hi.pop();
-        }
-    }
-    void balance(){
-        clean();
-        while(lo_sz<_k&&!hi.empty()){lo.push(hi.top());hi.pop();lo_sz++,hi_sz--;}
-        while(lo_sz>_k){hi.push(lo.top());lo.pop();lo_sz--;hi_sz++;}
-        clean();
-    }
-public:
-    DualHeap()=default;
-    DualHeap(int k):_k(k){}
-    void insert(T x){
-        if(!lo.empty()&&x<=lo.top())lo.push(x),lo_sz++;
-        else hi.push(x),hi_sz++;
-        sz++;
-        balance();
-    }
-    void erase(T x){
-        dead[x]++;
-        sz--;
-        if(!lo.empty()&&x<=lo.top()) lo_sz--;
-        else hi_sz--;
-        balance();
-    }
-    void set_k(int k){_k=k;balance();}
-    T qry() {clean();return lo.top();}
-    int size() const{return sz;}
-};
-
 //1.带删堆
 template <class T = int, class Cmp = less<T>> 
 class ErasableHeap {
@@ -138,9 +94,10 @@ public:
         lo.erase(it);
         return true;
     }
-    optional<T> qry() const {
+    //最大的 k 个元素之和,第 k 大元素
+    optional<pair<T, T>> qry() const {
         if(hi.size() < k) return nullopt;
-        return sum;
+        return {sum, *hi.begin()};
     }
 };
 
