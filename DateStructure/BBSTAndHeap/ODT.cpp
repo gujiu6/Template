9c3f2fd41679578602a04da40312f419aff773ac
gujiu6 <18273655445@163.com>
2026-08-24 22:35:00 +0800

    11

--- DateStructure/BBSTAndHeap/ODT.cpp
+++ DateStructure/BBSTAndHeap/ODT.cpp
@@ -1,144 +1,124 @@
 //ODT珂朵莉树
 #include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
-using ll = long long;
+using i64 = long long;
 
 
-template<typename T = ll>
+template <class T = int>
 class ODT {
 private:
     struct Node {
         int l, r;
-        mutable T val;
-        Node(int l, int r, T val): l(l), r(r), val(val) {}
-        bool operator< (const Node& other)const {
+        T v;
+        bool operator<(const Node &other) const {
             return l < other.l;
         }
     };
+    int n;
     set<Node> s;
-    auto split(int pos) {
-        auto it = prev(s.upper_bound(Node(pos, 0, 0)));
-        if(it->l == pos)
-            return it;
-        int l = it->l;
-        int r = it->r;
-        T v = it->val;
-        s.erase(it);
-        s.insert(Node(l, pos - 1, v));
-        return s.insert(Node(pos, r, v)).first;
-    }
-
-    auto merge(auto it) {
-        if(it != s.begin()) {
-            auto pre = prev(it);
-            if(pre->r + 1 == it->l && pre->val == it->val){
-                int l = pre->l;
-                int r = it->r;
-                T v = it->val;
-                s.erase(pre);
-                s.erase(it);
-                it = s.insert(Node(l, r, v)).first;
+    map<T, int> cnt;       // cnt[c] = 颜色 c 的元素总数
+    map<T, int> blocks;    // blocks[c] = 颜色 c 的不相交连续区间数量
+public:
+    ODT(const vector<T> &a): n(a.size() - 1) {
+        for (int l = 1, r; l <= n; l = r + 1) {
+            r = l;
+            while(r < n && a[r + 1] == a[l]) {
+                r++;
             }
+            s.insert({l, r, a[l]});
+            cnt[a[l]] += r - l + 1;
+            blocks[a[l]]++;
         }
-        auto nxt = next(it);
-        if(nxt!=s.end()) {
-            if(it->r + 1 == nxt->l && it->val == nxt->val) {
-                int l = it->l;
-                int r = nxt->r;
-                T v = it->val;
-                s.erase(it);
-                s.erase(nxt);
-                it = s.insert(Node(l, r, v)).first;
+    }
+    // 将包含 x 的区间拆开;返回左端点为 x 的区间
+    auto split(int x) {
+        if (x > n) {
+            return s.end();
+        }
+        auto it = prev(s.upper_bound({x, 0, {}}));
+        if (it->l == x) {
+            return it;
+        }
+        auto [l, r, v] = *it;
+        s.erase(it);
+        blocks[v]++;
+        s.insert({l, x - 1, v});
+        return s.insert({x, r, v}).first;
+    }
+    // 合并 it 与后面的所有相邻同色区间
+    auto mergeRight(typename set<Node>::iterator it) {
+        while (true) {
+            auto nxt = next(it);
+            if (nxt == s.end() ||
+                it->r + 1 != nxt->l ||
+                it->v != nxt->v) {
+                break;
             }
+            auto [l1, r1, v] = *it;
+            auto [l2, r2, _] = *nxt;
+            s.erase(it);
+            s.erase(nxt);
+            blocks[v]--;
+            it = s.insert({l1, r2, v}).first;
         }
         return it;
     }
+    // 将闭区间 [l, r] 全部染成 v
+    void assign(int l, int r, const T &v) {
+        auto itr = split(r + 1);
+        auto itl = split(l);
+        // 删除 [l,r] 内所有旧区间的贡献
+        for (auto it = itl; it != itr; ++it) {
+            int len = it->r - it->l + 1;
 
-    T power(T a, ll b, T mod) {
-        T ans=1;
-        while(b) {
-            if(b & 1) ans = ans * a % mod;
-            a = a * a % mod;
-            b >>= 1;
+            cnt[it->v] -= len;
+            blocks[it->v]--;
         }
-        return ans;
-    }
-public:
-    ODT(){}
-    ODT(int n, T v) {
-        s.insert(Node(1, n, v));
-    }
-    ODT(vector<T>& arr) {
-        int n = arr.size() - 1;
-        int l = 1;
-        for(int i = 2; i <= n + 1; i++) {
-            if(i == n + 1 || arr[i] != arr[l]) {
-                s.insert(Node(l, i-1, arr[l]));
-                l = i;
+        // 删除原区间
+        s.erase(itl, itr);
+        // 插入新区间
+        auto it = s.insert({l, r, v}).first;
+        cnt[v] += r - l + 1;
+        blocks[v]++;
+        // 和左右同色区间合并
+        if (it != s.begin()) {
+            auto pre = prev(it);
+            if (pre->r + 1 == it->l && pre->v == it->v) {
+                int nl = pre->l;
+                int nr = it->r;
+
+                s.erase(pre);
+                s.erase(it);
+
+                blocks[v]--;
+
+                it = s.insert({nl, nr, v}).first;
             }
         }
+        it = mergeRight(it);
     }
-
-    void assign(int l, int r, T v) {
-        auto R = split(r + 1);
-        auto L = split(l);
-        s.erase(L, R);
-        auto it = s.insert(Node(l , r, v)).first;
-        merge(it);
-    }
-
-    void add(int l, int r, T v) {
-        auto R = split(r + 1);
-        auto L = split(l);
-        for(auto it = L; it != R; it++) {
-            it->val += v;
+    // 查询颜色 c 的元素总数
+    int count(const T &c) const {
+        auto it = cnt.find(c);
+        if (it == cnt.end()) {
+            return 0;
         }
+        return it->second;
     }
-    T qry(int pos) {
-        auto it = prev(s.upper_bound(Node(pos, 0, 0)));
-        return it->val;
-    }
-
-    T sum(int l, int r) {
-        T ans = 0;
-        auto R = split(r + 1);
-        auto L = split(l);
-        for(auto it = L; it != R; it++){
-            ans += (T)(it->r - it->l + 1) * it->val;
+    // 查询颜色 c 有多少个不相交连续区间
+    int countBlocks(const T &c) const {
+        auto it = blocks.find(c);
+        if (it == blocks.end()) {
+            return 0;
         }
-        return ans;
+        return it->second;
     }
-
-    T kth(int l, int r, int k) {
-        vector<pair<T, int>> v;
-        auto R = split(r + 1);
-        auto L = split(l);
-        for(auto it = L; it != R; it++){
-            v.push_back({it->val, it->r - it->l + 1});
+    vector<T> values() const {
+        vector<T> a(n + 1);
+        for (auto [l, r, v] : s) {
+            fill(a.begin() + l, a.begin() + r + 1, v);
         }
-        sort(v.begin(), v.end());
-        for(auto [x, cnt] : v){
-            if(k <= cnt)
-                return x;
-            k -= cnt;
-        }
-        return T();
+        return a;
     }
-
-    T power_sum(int l, int r, ll k, ll mod) {
-        T ans = 0;
-        auto R = split(r + 1);
-        auto L = split(l);
-        for(auto it = L; it != R; it++){
-            ans = (ans + (T)(it->r - it->l + 1) * power(it->val, k, mod)) % mod;
-        }
-        return ans;
-    }
-
-    void debug() {
-        for(auto [l, r, v]:s) {
-            cout << "[" << l << "," << r << "] " << v << endl;
-        }
-    }
-
 };
