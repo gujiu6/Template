4c5c95e1346d2b3a5121affa60851022c4bc512c
gujiu6 <18273655445@163.com>
2026-08-30 13:20:42 +0800

    11

--- Common/PrefixAndDiff.cpp
+++ Common/PrefixAndDiff.cpp
@@ -13,24 +13,26 @@ using i64 = long long;
 
 //1.二维前缀和
 template <class T = i64>
-struct Prefix {
+class Prefix {
+public:
     int n, m;
     vector<vector<T>> s;
     Prefix(const vector<vector<T>>& a): n(a.size() - 1), m(a[0].size() - 1), s(n + 1, vector<T>(m + 1)) {
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
-                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j];
+                s[i][j] = (s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j]);
             }
         }
     }
     T sum(int x1, int y1, int x2, int y2)const {
-        return s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1];
+        return (s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1]);
     }
 };
 
 //2.二维差分
 template <class T = i64>
-struct Diff {
+class Diff {
+public:
     int n, m;
     vector<vector<T>> d;
     Diff(int n, int m): n(n), m(m), d(n + 2, vector<T>(m + 2)) {}
@@ -46,7 +48,7 @@ struct Diff {
         vector<vector<T>> a(n + 1, vector<T>(m + 1));
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
-                a[i][j] = d[i][j] + d[i][j - 1] + d[i - 1][j] - d[i - 1][j - 1];
+                a[i][j] = (d[i][j] + d[i][j - 1] + d[i - 1][j] - d[i - 1][j - 1]);
             }
         }
         return a;
@@ -70,12 +72,14 @@ vector<T> kthDiff(vector<T> a, int k) {
 
 //4.等差数列差分
 template <class T = i64>
-struct ArithmeticDiff {
+class ArithmeticDiff {
+public:
     int n;
     vector<T> dd;//二阶差分
     ArithmeticDiff (int n): n(n), dd(n + 2) {}
     //[l,r]加等差数列初始s,公差d
     void add(int l, int r, T s, T d) {
+        if(l > r) return;
         int len = (r - l + 1);
         T t = s + (len - 1) * d;
         dd[l] += s;
@@ -86,7 +90,7 @@ struct ArithmeticDiff {
     vector<T> build() {
         vector<T> a(n + 1);
         for(int i = 1; i <= n; i++) {
-            dd[i] += dd[i - 1];
+            dd[i] = (dd[i] + dd[i - 1]);
         }
         for(int i = 1; i <= n; i++) {
             a[i] = a[i - 1] + dd[i];
@@ -126,7 +130,8 @@ T maxSubMat(const vector<vector<T>>& a) {
 
 //6.所有区间 gcd
 template <class T = i64>
-struct gcdRanges {
+class gcdRanges {
+public:
     int n;
     vector<vector<pair<int, T>>> g;
     gcdRanges (const vector<T>& a): n(a.size() - 1), g(n + 1) {
